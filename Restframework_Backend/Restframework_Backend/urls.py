55f95d94421ebc6571cdbb2430c5b6cb36978873
10546027Jun <10546027@ntub.edu.tw>
2019-05-30 01:26:39 +0800

    使用Router定義url

--- Restframework_Backend/Restframework_Backend/urls.py
+++ Restframework_Backend/Restframework_Backend/urls.py
@@ -1,28 +1,34 @@
 from django.contrib import admin
 from django.urls import path,include
 from coupon_data import views
+from coupon_data import viewset
 from rest_framework.routers import DefaultRouter
 
-# router = DefaultRouter()
-# router.register(r'coupons', CouponList)
+router = DefaultRouter()
+router.register(r'coupons', viewset.CouponViewSet)
+router.register(r'users',viewset.UserViewSet)
+
+# ---------------using router----------------------------------------
+urlpatterns = [
+    path('',include(router.urls)),
+
+    #restframe work admin
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    # django admin
+    # path('admin/', admin.site.urls),
+]
+# ---------------using router----------------------------------------
 
 
+# ---------------using api_root----------------------------------------
 # urlpatterns = [
-#     path('',views.api_root,name = 'coupons'),
-#     path('api',views.api_root,name='users'),
-#     path('api/coupons/', views.CouponList.as_view()),
-#     path('api/coupons/<coupon_id>/',views.CouponDetail.as_view()),
-#     path('api/users/',views.UserList.as_view()),
+#     path('',views.api_root),
+#     path('api/coupons/', views.CouponList.as_view(),name = 'coupons'),
+#     path('api/coupons/<coupon_id>/',views.CouponDetail.as_view(),name = 'coupon-detail'),
+#     path('api/users/',views.UserList.as_view(),name = 'users'),
+#     path('api/users/<user_id>/',views.UserDetail.as_view(),name='user-detail'),
 #     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 #     # path('admin/', admin.site.urls),
 # ]
-
-urlpatterns = [
-    path('',views.api_root),
-    path('api/coupons/', views.CouponList.as_view(),name = 'coupons'),
-    path('api/coupons/<coupon_id>/',views.CouponDetail.as_view(),name = 'coupon-detail'),
-    path('api/users/',views.UserList.as_view(),name = 'users'),
-    path('api/users/<user_id>/',views.UserDetail.as_view(),name='user-detail'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # path('admin/', admin.site.urls),
-]
+# ---------------using api_root----------------------------------------
